2082a71c59c14907243789acb08be5091700d54a
Obaid Ur-Rahmaan <oxu711@cs.bham.ac.uk>
2020-07-02 16:54:43 +0100

    Added Signup route

--- kin-io-server/functions/index.js
+++ kin-io-server/functions/index.js
@@ -1,15 +1,29 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
-
+const app = require("express")();
 admin.initializeApp();
 
-const express = require("express");
-const app = express();
+var firebaseConfig = {
+  apiKey: "AIzaSyAg8Q-hzszN8AhqpnXaY9Wfuz6ClypPF-Q",
+  authDomain: "kin-io.firebaseapp.com",
+  databaseURL: "https://kin-io.firebaseio.com",
+  projectId: "kin-io",
+  storageBucket: "kin-io.appspot.com",
+  messagingSenderId: "762009128726",
+  appId: "1:762009128726:web:81ac726a2b5de78289ca37",
+  measurementId: "G-82LB09ZW44",
+};
+
+const firebase = require("firebase");
+firebase.initializeApp(firebaseConfig);
+
+const db = admin.firestore();
+
+//  API Routes
+// --------------------------------------------------------------------------------------------
 
 app.get("/posts", (request, response) => {
-  admin
-    .firestore()
-    .collection("posts")
+  db.collection("posts")
     .orderBy("createdAt", "desc")
     .get()
     .then((data) => {
@@ -36,9 +50,7 @@ app.post("/post", (request, response) => {
     createdAt: new Date().toISOString(),
   };
 
-  admin
-    .firestore()
-    .collection("posts")
+  db.collection("posts")
     .add(newPost)
     .then((doc) => {
       response.json({ message: `document ${doc.id} created successfully` });
@@ -46,6 +58,89 @@ app.post("/post", (request, response) => {
     .catch((err) => {
       response.status(500).json({ error: "something went wrong" });
       console.error(err);
+    });
+});
+
+const isEmail = (email) => {
+  const emailRegEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (email.match(emailRegEx)) {
+    return true;
+  }
+
+  return false;
+};
+
+const isEmpty = (string) => {
+  if (string.trim() == "") {
+    return true;
+  }
+
+  return false;
+};
+
+// Signup route
+app.post("/signup", (request, response) => {
+  const newUser = {
+    email: request.body.email,
+    password: request.body.password,
+    confirmPassword: request.body.confirmPassword,
+    handle: request.body.handle,
+  };
+
+  let errors = {};
+
+  if (isEmpty(newUser.email)) {
+    errors.email = "Must not be empty";
+  } else if (!isEmail(newUser.email)) {
+    errors.email = "Must be a valid email address";
+  }
+
+  if (isEmpty(newUser.password)) errors.password = "Must not be empty";
+  if (newUser.password !== newUser.confirmPassword) {
+    errors.confirmPassword = "Passwords must match";
+  }
+  if (isEmpty(newUser.handle)) errors.handle = "Must not be empty";
+
+  if (Object.keys(errors).length > 0) return response.status(400).json(errors);
+
+  let token, userId;
+  db.doc(`/users/${newUser.handle}`)
+    .get()
+    .then((doc) => {
+      if (doc.exists) {
+        return response
+          .status(400)
+          .json({ handle: "This handle is already taken" });
+      } else {
+        return firebase
+          .auth()
+          .createUserWithEmailAndPassword(newUser.email, newUser.password);
+      }
+    })
+    .then((data) => {
+      userId = data.user.uid;
+      return data.user.getIdToken();
+    })
+    .then((idToken) => {
+      token = idToken;
+      const userCredentials = {
+        handle: newUser.handle,
+        email: newUser.email,
+        createdAt: new Date().toISOString(),
+        userId,
+      };
+
+      return db.doc(`/users/${newUser.handle}`).set(userCredentials);
+    })
+    .then(() => {
+      return response.status(201).json({ token });
+    })
+    .catch((err) => {
+      console.error(err);
+      if (err.code === "auth/email-already-in-use") {
+        return response.status(400).json({ email: "Email is already in use" });
+      }
+      return response.status(500).json({ error: err.code });
     });
 });
 
