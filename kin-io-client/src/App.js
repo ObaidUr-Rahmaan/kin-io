4d5c4e792ee4eef5fad1710c9a4d17f32f6e7ae2
Obaid Ur-Rahmaan <oxu711@cs.bham.ac.uk>
2020-07-04 00:14:00 +0100

    Front-end: Implemented Sign-up page

--- kin-io-client/src/App.js
+++ kin-io-client/src/App.js
@@ -25,8 +25,34 @@ const theme = createMuiTheme({
       dark: "#424242",
     },
   },
-  typography: {
-    useNextVariants: true,
+  styles: {
+    typography: {
+      useNextVariants: true,
+    },
+    form: {
+      textAlign: "center",
+    },
+    image: {
+      margin: "20px auto 20px auto",
+    },
+    pageTitle: {
+      margin: "10px auto 10px auto",
+    },
+    textField: {
+      margin: "10px auto 10px auto",
+    },
+    button: {
+      marginTop: 20,
+      position: "relative",
+    },
+    customError: {
+      color: "red",
+      fontSize: "0.8rem",
+      marginTop: 10,
+    },
+    progress: {
+      position: "absolute",
+    },
   },
 });
 
--- kin-io-client/src/pages/login.js
+++ kin-io-client/src/pages/login.js
@@ -12,32 +12,9 @@ import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import CircularProgress from "@material-ui/core/CircularProgress";
 
-const styles = {
-  form: {
-    textAlign: "center",
-  },
-  image: {
-    margin: "20px auto 20px auto",
-  },
-  pageTitle: {
-    margin: "10px auto 10px auto",
-  },
-  textField: {
-    margin: "10px auto 10px auto",
-  },
-  button: {
-    marginTop: 20,
-    position: "relative",
-  },
-  customError: {
-    color: "red",
-    fontSize: "0.8rem",
-    marginTop: 10,
-  },
-  progress: {
-    position: "absolute",
-  },
-};
+const styles = (theme) => ({
+  ...theme.styles,
+});
 
 class login extends Component {
   constructor(props) {
@@ -64,6 +41,7 @@ class login extends Component {
       .post("/login", userData)
       .then((result) => {
         console.log(result.data);
+        localStorage.setItem("FBIdToken", `Bearer ${result.data.token}`);
         this.setState({
           loading: false,
         });
@@ -144,7 +122,7 @@ class login extends Component {
             </Button>
             <br />
             <small>
-              Don't have an account? Sign-up <Link to="/signup">here</Link>
+              Don't have an account? Signup <Link to="/signup">here</Link>
             </small>
           </form>
         </Grid>
