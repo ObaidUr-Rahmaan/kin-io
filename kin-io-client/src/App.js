a691e08933bd3083af85d78d5060250cbef84a35
Obaid Ur-Rahmaan <oxu711@cs.bham.ac.uk>
2020-07-04 00:29:04 +0100

    Front-end: Implemented Authentication routes to refresh token on login timeout

--- kin-io-client/src/App.js
+++ kin-io-client/src/App.js
@@ -3,58 +3,32 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import { ThemeProvider as MuiThemeProvider } from "@material-ui/core/styles";
 import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
+import themeFile from "./util/theme";
+import jwtDecode from "jwt-decode";
 
 // Components
 import NavBar from "./components/NavBar";
+import AuthRoute from "./util/AuthRoute.js";
 
 // Pages
 import home from "./pages/home";
 import login from "./pages/login";
 import signup from "./pages/signup";
 
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      main: "#212121",
-      light: "#616161",
-      dark: "#424242",
-    },
-    secondary: {
-      main: "#212121",
-      light: "#616161",
-      dark: "#424242",
-    },
-  },
-  styles: {
-    typography: {
-      useNextVariants: true,
-    },
-    form: {
-      textAlign: "center",
-    },
-    image: {
-      margin: "20px auto 20px auto",
-    },
-    pageTitle: {
-      margin: "10px auto 10px auto",
-    },
-    textField: {
-      margin: "10px auto 10px auto",
-    },
-    button: {
-      marginTop: 20,
-      position: "relative",
-    },
-    customError: {
-      color: "red",
-      fontSize: "0.8rem",
-      marginTop: 10,
-    },
-    progress: {
-      position: "absolute",
-    },
-  },
-});
+const theme = createMuiTheme(themeFile);
+
+const token = localStorage.FBIdToken;
+
+let authenticated;
+if (token) {
+  const decodedToken = jwtDecode(token);
+  if (decodedToken.exp * 1000 < Date.now()) {
+    window.location.href = "/login";
+    authenticated = false;
+  } else {
+    authenticated = true;
+  }
+}
 
 class App extends Component {
   render() {
@@ -66,8 +40,18 @@ class App extends Component {
             <div className="container">
               <Switch>
                 <Route exact path="/" component={home} />
-                <Route exact path="/login" component={login} />
-                <Route exact path="/signup" component={signup} />
+                <AuthRoute
+                  exact
+                  path="/login"
+                  component={login}
+                  authenticated={authenticated}
+                />
+                <AuthRoute
+                  exact
+                  path="/signup"
+                  component={signup}
+                  authenticated={authenticated}
+                />
               </Switch>
             </div>
           </Router>
---
+++ kin-io-client/src/util/theme.js
@@ -0,0 +1,43 @@
+export default {
+  palette: {
+    primary: {
+      main: "#212121",
+      light: "#616161",
+      dark: "#424242",
+    },
+    secondary: {
+      main: "#212121",
+      light: "#616161",
+      dark: "#424242",
+    },
+  },
+  styles: {
+    typography: {
+      useNextVariants: true,
+    },
+    form: {
+      textAlign: "center",
+    },
+    image: {
+      margin: "20px auto 20px auto",
+    },
+    pageTitle: {
+      margin: "10px auto 10px auto",
+    },
+    textField: {
+      margin: "10px auto 10px auto",
+    },
+    button: {
+      marginTop: 20,
+      position: "relative",
+    },
+    customError: {
+      color: "red",
+      fontSize: "0.8rem",
+      marginTop: 10,
+    },
+    progress: {
+      position: "absolute",
+    },
+  },
+};
