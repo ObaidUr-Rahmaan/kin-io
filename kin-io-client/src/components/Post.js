5139151cb0b1482a42b3a8edc72edea903cb6791
Obaid Ur-Rahmaan <oxu711@cs.bham.ac.uk>
2020-07-04 21:12:42 +0100

    Front-end: Added Delete button

--- kin-io-client/src/components/Post.js
+++ kin-io-client/src/components/Post.js
@@ -5,6 +5,7 @@ import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import PropTypes from "prop-types";
 import MyButton from "../util/MyButton";
+import DeletePost from "./DeletePost";
 
 // MUI Stuff
 import Card from "@material-ui/core/Card";
@@ -70,7 +71,10 @@ class Post extends Component {
         likeCount,
         commentCount,
       },
-      user: { authenticated },
+      user: {
+        authenticated,
+        credentials: { handle },
+      },
     } = this.props;
     const likeButton = !authenticated ? (
       <MyButton tip="Like">
@@ -87,6 +91,10 @@ class Post extends Component {
         <FavoriteBorder color="primary" />
       </MyButton>
     );
+    const deleteButton =
+      authenticated && userHandle === handle ? (
+        <DeletePost postId={postId} />
+      ) : null;
     return (
       <Card className={classes.card}>
         <CardMedia
@@ -103,6 +111,7 @@ class Post extends Component {
           >
             {userHandle}
           </Typography>
+          {deleteButton}
           <Typography variant="body2" color="textSecondary">
             {dayjs(createdAt).fromNow()}
           </Typography>
